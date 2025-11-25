dda04cf467ce11271e296ba719e93e09cb3b3e9c
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-11-25 14:46:56 +0100

    feat: Making Sense of Tuples

--- 2-TypeScript-Basics-&-Basic-Types/arrays-objects.ts
+++ 2-TypeScript-Basics-&-Basic-Types/arrays-objects.ts
@@ -8,3 +8,8 @@ let users: Array<string | number>;
 users = [1, "Max"];
 users = [5, 1];
 users = ["Max", "Anna"];
+
+let possibleResults: [number, number]; // [1, -1]
+
+possibleResults = [1, -1];
+possibleResults = [5, 10, 12];
