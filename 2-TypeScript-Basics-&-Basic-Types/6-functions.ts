a6827bdedbc9cf6fe221325bd901d384ba7f9965
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-11-26 14:34:46 +0100

    feat: Functions As Types

--- 2-TypeScript-Basics-&-Basic-Types/6-functions.ts
+++ 2-TypeScript-Basics-&-Basic-Types/6-functions.ts
@@ -10,3 +10,31 @@ function logAndThrow(errorMessage: string): never {
     console.log(errorMessage);
     throw new Error(errorMessage);
 }
+
+const logMsg = (msg: string) => {
+    console.log(msg);
+}
+
+function performJob(cb: (msg: string) => void) {
+    // ...
+    cb("Job done!");
+}
+
+performJob(log);
+
+type User = {
+    name: string;
+    age: number;
+    greet: () => string;
+}
+
+let user: User = {
+    name: "Max",
+    age: 39,
+    greet() {
+        console.log("Hello there!");
+        return this.name;
+    }
+}
+
+user.greet();
