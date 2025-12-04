8e1b8ae784619875c42862812ef2c55b393c4843
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-12-04 17:51:20 +0100

    feat: Getting Started & First Steps

---
+++ 4-TypeScript-Essentials-Demo-Project/calculator.ts
@@ -0,0 +1,15 @@
+// data:
+// initial amount
+// annual contribution
+// expected return
+// duration
+
+function calulateInvestment(data) {} // => result[]
+
+function printResults(results) {
+    // print (output) the result data
+}
+
+const results = calulateInvestment(...)
+
+printResults(results);
