152345f3a71012ddd5d72e21e885fc7820152f76
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-12-17 17:25:27 +0100

    feat: Arrow Functions

--- 5-Next-Generation-JavaScript-&-TypeScript/src/app.ts
+++ 5-Next-Generation-JavaScript-&-TypeScript/src/app.ts
@@ -4,14 +4,29 @@ let age = 30;
 
 age = 29;
 
-function add(a: number, b: number) {
-  let result; // scope is the same for var => only global and function scopes
-  result = a + b;
-  return result;
-}
+// function add(a: number, b: number) {
+//   let result; // scope is the same for var => only global and function scopes
+//   result = a + b;
+//   return result;
+// }
 
 // if (age > 20) {
 //   let isOld = true;
 // }
 //
 // console.log(isOld);
+
+const add = (a: number, b: number) => a + b;
+
+console.log(add(2, 5));
+
+const printOutput: (a: number | string) => void = (output) =>
+  console.log(output);
+
+const button = document.querySelector("button");
+
+if (button) {
+  button.addEventListener("click", (event) => console.log(event));
+}
+
+printOutput(add(5, 2));
