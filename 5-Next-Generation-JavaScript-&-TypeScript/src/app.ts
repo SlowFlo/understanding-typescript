ac621336494a55b80f346f46fde06d5dcabaaaa1
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-12-17 17:11:53 +0100

    feat: "let" and "const"

--- 5-Next-Generation-JavaScript-&-TypeScript/src/app.ts
+++ 5-Next-Generation-JavaScript-&-TypeScript/src/app.ts
@@ -1,1 +1,17 @@
-// Code goes here!
+const userName = "Max";
+// userName = "Maximilian";
+let age = 30;
+
+age = 29;
+
+function add(a: number, b: number) {
+  let result; // scope is the same for var => only global and function scopes
+  result = a + b;
+  return result;
+}
+
+// if (age > 20) {
+//   let isOld = true;
+// }
+//
+// console.log(isOld);
