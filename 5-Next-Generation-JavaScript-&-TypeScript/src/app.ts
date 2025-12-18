37b61da3f5082cae4746897d902f1a221300123c
Florian BAUTRY <florian-f-externe.bautry@edf.fr>
2025-12-18 18:54:59 +0100

    feat: Rest Parameters

--- 5-Next-Generation-JavaScript-&-TypeScript/src/app.ts
+++ 5-Next-Generation-JavaScript-&-TypeScript/src/app.ts
@@ -16,20 +16,20 @@ age = 29;
 //
 // console.log(isOld);
 
-const add = (a: number, b: number = 1) => a + b;
-
-console.log(add(2, 5));
-
-const printOutput: (a: number | string) => void = (output) =>
-  console.log(output);
-
-const button = document.querySelector("button");
-
-if (button) {
-  button.addEventListener("click", (event) => console.log(event));
-}
-
-printOutput(add(5));
+// const add = (a: number, b: number = 1) => a + b;
+//
+// console.log(add(2, 5));
+//
+// const printOutput: (a: number | string) => void = (output) =>
+//   console.log(output);
+//
+// const button = document.querySelector("button");
+//
+// if (button) {
+//   button.addEventListener("click", (event) => console.log(event));
+// }
+//
+// printOutput(add(5));
 
 const hobbies = ["Sports", "Cooking"];
 const activeHobbies = ["Hiking"];
@@ -42,3 +42,13 @@ const person = {
 };
 
 const copiedPerson = { ...person };
+
+// const add = (...numbers: [number, number, number]) => {
+const add = (...numbers: number[]) => {
+  return numbers.reduce((curResult, curValue) => {
+    return curResult + curValue;
+  }, 0);
+};
+
+const addedNumbers = add(5, 10, 2, 3.7);
+console.log(addedNumbers);
